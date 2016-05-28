5e1f447caded53f0f63fae4667f9df0f1f8f46ca
OXullo Intersecans <x@brainrapers.org>
2016-05-28 19:36:33 +0200

    Added copyright headers

--- src/filter.h
+++ src/filter.h
@@ -1,3 +1,26 @@
+/*
+Arduino-MAX30100 oximetry / heart rate integrated sensor library
+Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Based on https://www.researchgate.net/publication/3728672_A_simple_real-time_QRS_detection_algorithm
+// http://www.atmel.com/images/doc2527.pdf
+// http://de.mathworks.com/matlabcentral/fileexchange/45840-complete-pan-tompkins-implementation-ecg-qrs-detector/content/pan_tompkin.m
+// http://cnx.org/contents/YR1BUs9_@1/QRS-Detection-Using-Pan-Tompki
+
 #ifndef FILTER_H
 #define FILTER_H
 
