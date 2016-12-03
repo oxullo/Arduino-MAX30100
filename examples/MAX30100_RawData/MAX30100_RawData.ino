8aa327f195bd790d1e22380af401d130e1303bd7
OXullo Intersecans <x@brainrapers.org>
2016-12-03 10:55:47 +0100

    Add an example that shows a possible usage pattern of the sensor driver

---
+++ examples/MAX30100_RawData/MAX30100_RawData.ino
@@ -0,0 +1,72 @@
+/*
+Arduino-MAX30100 oximetry / heart rate integrated sensor library
+Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// The example shows how to retrieve raw values from the sensor
+// experimenting with the most relevant configuration parameters.
+// Use the "Serial Plotter" app from arduino IDE 1.6.7+ to plot the output
+
+#include <Wire.h>
+#include "MAX30100.h"
+
+// Tweakable parameters
+// Sampling and polling frequency must be set consistently
+#define POLL_PERIOD_US                      1E06 / 100
+#define SAMPLING_RATE                       MAX30100_SAMPRATE_100HZ
+
+// The LEDs currents must be set to a level that avoids clipping and maximises the
+// dynamic range
+#define IR_LED_CURRENT                      MAX30100_LED_CURR_50MA
+#define RED_LED_CURRENT                     MAX30100_LED_CURR_27_1MA
+
+// The pulse width of the LEDs driving determines the resolution of
+// the ADC (which is a Sigma-Delta).
+// set HIGHRES_MODE to true only when setting PULSE_WIDTH to MAX30100_SPC_PW_1600US_16BITS
+#define PULSE_WIDTH                         MAX30100_SPC_PW_1600US_16BITS
+#define HIGHRES_MODE                        true
+
+
+// Instantiate a MAX30100 sensor class
+MAX30100 sensor;
+uint32_t tsLastPollUs = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Set up the wanted parameters
+    sensor.begin();
+    sensor.setMode(MAX30100_MODE_SPO2_HR);
+    sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT);
+    sensor.setLedsPulseWidth(PULSE_WIDTH);
+    sensor.setSamplingRate(SAMPLING_RATE);
+    sensor.setHighresModeEnabled(HIGHRES_MODE);
+}
+
+void loop()
+{
+    // Using this construct instead of a delay allows to account for the time
+    // spent sending data thru the serial and tighten the timings with the sampling
+    if (micros() - tsLastPollUs > POLL_PERIOD_US) {
+        sensor.update();
+        tsLastPollUs = micros();
+
+        Serial.print(sensor.rawIRValue);
+        Serial.print('\t');
+        Serial.println(sensor.rawRedValue);
+    }
+}
