a721884d0d5c43a3a8033b978f2e56edd8b7bd30
Shivam Gupta <gupta.shivam1996@gmail.com>
2020-09-26 23:21:09 +0530

    Initial commit for MAX30102

--- src/MAX30102_SpO2Calculator.cpp
+++ src/MAX30102_SpO2Calculator.cpp
@@ -1,6 +1,9 @@
 /*
-Arduino-MAX30100 oximetry / heart rate integrated sensor library
-Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
+Arduino-MAX30102 oximetry / heart rate integrated sensor library by Shivam Gupta (gupta.shivam1996@gmail.com)
+
+Based on MAX30100 library, Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
+All alogrithms and methods used are from the above author,
+I have only modified this enough to make it work with the new MAX30102 sensor.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <math.h>
 
-#include "MAX30100_SpO2Calculator.h"
+#include "MAX30102_SpO2Calculator.h"
 
 // SaO2 Look-up Table
 // http://www.ti.com/lit/an/slaa274b/slaa274b.pdf
